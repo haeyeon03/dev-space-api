ed489bd9de91371733051036517c5433f4752be9
pcs0801 <forall36@nate.com>
2025-08-06 14:24:09 +0900

    feat: BoardPostRequestDto 클래스 생성

---
+++ src/main/java/kh/devspaceapi/model/dto/boardPost/BoardPostRequestDto.java
@@ -0,0 +1,7 @@
+package kh.devspaceapi.model.dto.boardPost;
+
+import kh.devspaceapi.model.dto.base.BasePageRequestDto;
+
+public class BoardPostRequestDto extends BasePageRequestDto {
+	String it = "is test";
+}

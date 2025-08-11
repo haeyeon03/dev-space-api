141a142e25b72e333f4271357958bb235b29180b
HaeYeon Lee <haeyeon.dev@gmail.com>
2025-08-11 10:07:33 +0900

    refactor: NewsPost 수정

--- src/main/java/kh/devspaceapi/model/dto/newsPost/NewsPostRequestDto.java
+++ src/main/java/kh/devspaceapi/model/dto/newsPost/NewsPostRequestDto.java
@@ -1,11 +1,15 @@
 package kh.devspaceapi.model.dto.newsPost;
 
 import kh.devspaceapi.model.dto.base.BasePageRequestDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewsPostRequestDto extends BasePageRequestDto {
 
 	// BasePageRequestDto에서 curPage, pageSize 사용 
